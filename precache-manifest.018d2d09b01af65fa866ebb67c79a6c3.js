5313fab471920e3f0a3a6c426b5927225dc96329
Your Name <vladedumail@gmail.com>
2021-02-05 16:10:17 +0200

    builded with homepage at package.json file

---
+++ precache-manifest.018d2d09b01af65fa866ebb67c79a6c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058c574d61cf978294ff365a2ec8c25e",
+    "url": "/Chuck-Norris-API-React/index.html"
+  },
+  {
+    "revision": "d80f11187e9cb9491b52",
+    "url": "/Chuck-Norris-API-React/static/css/main.36dd79bd.chunk.css"
+  },
+  {
+    "revision": "3c3c42ad8dc0cede321b",
+    "url": "/Chuck-Norris-API-React/static/js/2.f39c240c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chuck-Norris-API-React/static/js/2.f39c240c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d80f11187e9cb9491b52",
+    "url": "/Chuck-Norris-API-React/static/js/main.ddba558f.chunk.js"
+  },
+  {
+    "revision": "1fa7c03b3cf24e6bf36b",
+    "url": "/Chuck-Norris-API-React/static/js/runtime-main.72281f19.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/Chuck-Norris-API-React/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Chuck-Norris-API-React/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Chuck-Norris-API-React/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "646742be2b02728edf9b9806fdf0dc99",
+    "url": "/Chuck-Norris-API-React/static/media/burger-menu.646742be.svg"
+  },
+  {
+    "revision": "b434679963008fc7f18fe334bfc44e53",
+    "url": "/Chuck-Norris-API-React/static/media/close_menu.b4346799.svg"
+  },
+  {
+    "revision": "9d3e2327fc3f5aa0d500064097591104",
+    "url": "/Chuck-Norris-API-React/static/media/heart_checked.9d3e2327.svg"
+  },
+  {
+    "revision": "63f9f86f36f4d6fcb43a976a0a4f45c6",
+    "url": "/Chuck-Norris-API-React/static/media/heart_unchecked.63f9f86f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d02d88142644f27d914fcaa32384353.js"
+  "/Chuck-Norris-API-React/precache-manifest.018d2d09b01af65fa866ebb67c79a6c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chuck-Norris-API-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
